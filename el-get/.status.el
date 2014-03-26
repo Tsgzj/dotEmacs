eae82ecf2f75ee102ab8a5a53c1183853bf44bff
Tsgzj <wxsun1991@gmail.com>
2014-03-26 17:32:21 +0800

    Changes to be committed: 	new file:   #.emacs# 	deleted:    .#.emacs 	modified:   .emacs 	modified:   backups/!Users!Sun!.ido.last~ 	modified:   el-get/.loaddefs.el 	modified:   el-get/.loaddefs.elc 	modified:   el-get/.status.el 	new file:   el-get/color-theme-ahei/color-theme-ahei.el 	new file:   el-get/color-theme-ahei/color-theme-ahei.elc 	new file:   el-get/color-theme-almost-monokai 	new file:   el-get/color-theme-solarized 	new file:   el-get/color-theme-tomorrow 	new file:   el-get/color-theme-zenburn 	new file:   el-get/color-theme/AUTHORS 	new file:   el-get/color-theme/BUGS 	new file:   el-get/color-theme/COPYING 	new file:   el-get/color-theme/ChangeLog 	new file:   el-get/color-theme/HACKING 	new file:   el-get/color-theme/Makefile 	new file:   el-get/color-theme/Makefile.defs 	new file:   el-get/color-theme/README 	new file:   el-get/color-theme/build-stamp 	new file:   el-get/color-theme/color-theme-6.6.0.tar.gz 	new file:   el-get/color-theme/color-theme-autoloads.el 	new file:   el-get/color-theme/color-theme-autoloads.elc 	new file:   el-get/color-theme/color-theme-autoloads.in 	new file:   el-get/color-theme/color-theme.el 	new file:   el-get/color-theme/color-theme.elc 	new file:   el-get/color-theme/configure-stamp 	new file:   el-get/color-theme/themes/color-theme-example.el 	new file:   el-get/color-theme/themes/color-theme-example.elc 	new file:   el-get/color-theme/themes/color-theme-library.el 	new file:   el-get/color-theme/themes/color-theme-library.elc 	new file:   el-get/monokai-theme/monokai-theme.el 	new file:   el-get/monokai-theme/monokai-theme.elc 	new file:   el-get/tomorrow-night-paradise-theme 	modified:   places 	new file:   themes/molokai-theme
     Changes not staged for commit:
    	modified:   el-get/ace-jump-mode (untracked content)
    	modified:   el-get/color-theme-almost-monokai (untracked content)
    	modified:   el-get/color-theme-tomorrow (untracked content)
    	modified:   el-get/dash (untracked content)
    	modified:   el-get/popup (untracked content)
    	modified:   el-get/smooth-scrolling (untracked content)
    	modified:   el-get/yascroll (untracked content)
    	modified:   plugins/markdown-mode (untracked content)
    	modified:   yasnippet (modified content, untracked content)

--- el-get/.status.el
+++ el-get/.status.el
@@ -2,6 +2,24 @@
                 (:name ace-jump-mode :website "https://github.com/winterTTr/ace-jump-mode/wiki" :description "A quick cursor location minor mode for emacs" :type github :pkgname "winterTTr/ace-jump-mode" :features ace-jump-mode))
  (cl-lib status "installed" recipe
          (:name cl-lib :builtin "24.3" :type elpa :description "Properly prefixed CL functions and macros" :url "http://elpa.gnu.org/packages/cl-lib.html"))
+ (color-theme status "installed" recipe
+              (:name color-theme :description "An Emacs-Lisp package with more than 50 color themes for your use. For questions about color-theme" :website "http://www.nongnu.org/color-theme/" :type http-tar :options
+                     ("xzf")
+                     :url "http://download.savannah.gnu.org/releases/color-theme/color-theme-6.6.0.tar.gz" :load "color-theme.el" :features "color-theme" :post-init
+                     (progn
+                       (color-theme-initialize)
+                       (setq color-theme-is-global t))))
+ (color-theme-almost-monokai status "installed" recipe
+                             (:name color-theme-almost-monokai :description "A beautiful, fruity, calm, yet dark color theme for Emacs." :type github :pkgname "lut4rp/almost-monokai" :depends color-theme :prepare
+                                    (autoload 'color-theme-almost-monokai "color-theme-almost-monokai" "color-theme: almost-monokai" t)))
+ (color-theme-tomorrow status "installed" recipe
+                       (:name color-theme-tomorrow :description "Emacs highlighting using Chris Charles's Tomorrow color scheme" :type github :pkgname "ccharles/Tomorrow-Theme" :depends color-theme :prepare
+                              (progn
+                                (autoload 'color-theme-tomorrow "GNU Emacs/color-theme-tomorrow" "color-theme: tomorrow" t)
+                                (autoload 'color-theme-tomorrow-night "GNU Emacs/color-theme-tomorrow" "color-theme: tomorrow-night" t)
+                                (autoload 'color-theme-tomorrow-night-eighties "GNU Emacs/color-theme-tomorrow" "color-theme: tomorrow-night-eighties" t)
+                                (autoload 'color-theme-tomorrow-night-blue "GNU Emacs/color-theme-tomorrow" "color-theme: tomorrow-night-blue" t)
+                                (autoload 'color-theme-tomorrow-night-bright "GNU Emacs/color-theme-tomorrow" "color-theme: tomorrow-night-bright" t))))
  (dash status "installed" recipe
        (:name dash :description "A modern list api for Emacs. No 'cl required." :type github :pkgname "magnars/dash.el"))
  (el-get status "installed" recipe
@@ -23,5 +41,9 @@
  (sr-speedbar status "installed" recipe
               (:name sr-speedbar :type emacswiki :description "Same frame speedbar" :post-init
                      (require 'sr-speedbar)))
+ (tomorrow-night-paradise-theme status "installed" recipe
+                                (:name tomorrow-night-paradise-theme :description "A light-on-dark Emacs theme which is essentially a tweaked version of Chris Kempson's Tomorrow Night Bright theme." :website "https://github.com/jimeh/tomorrow-night-paradise-theme.el" :type github :pkgname "jimeh/tomorrow-night-paradise-theme.el" :minimum-emacs-version 24 :post-init
+                                       (add-to-list 'custom-theme-load-path default-directory)))
+ (tomorrow-theme status "required" recipe nil)
  (yascroll status "installed" recipe
            (:name yascroll :description "Yet Another Scroll Bar Mode" :website "https://github.com/m2ym/yascroll-el" :type github :pkgname "m2ym/yascroll-el" :features "yascroll")))
